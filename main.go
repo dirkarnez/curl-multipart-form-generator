b1a078f3e95bca546ab79d0c4beffc6be69c045e
dirkarnez <smalldirkalex@gmail.com>
2024-05-13 16:15:32 +0800

    - wip

--- main.go
+++ main.go
@@ -1,24 +1,21 @@
 package main
 
 import (
+	"log"
+	"path/filepath"
+
 	"github.com/harry1453/go-common-file-dialog/cfd"
 	"github.com/harry1453/go-common-file-dialog/cfdutil"
-	"log"
 )
 
 func main() {
+	a := filepath.ToSlash(`C:\curl-multipart-form-generator\go.sum`)
+	log.Printf("Chosen file(s): %s\n", a)
+
 	results, err := cfdutil.ShowOpenMultipleFilesDialog(cfd.DialogConfig{
 		Title: "Open Multiple Files",
 		Role:  "OpenFilesExample",
 		FileFilters: []cfd.FileFilter{
-			{
-				DisplayName: "Text Files (*.txt)",
-				Pattern:     "*.txt",
-			},
-			{
-				DisplayName: "Image Files (*.jpg, *.png)",
-				Pattern:     "*.jpg;*.png",
-			},
 			{
 				DisplayName: "All Files (*.*)",
 				Pattern:     "*.*",
